8a54a6eadc62a2ae7a78b13ee2536cee2f58bc5a
agustinus <agustinus.kurniawan@reebonz.com>
2019-09-10 12:39:05 +0800

    Rename file to be more descriptive

---
+++ src/Domain/DataGovService.js
@@ -0,0 +1,14 @@
+import DomainCommon from './Common';
+
+const DataGovService = {
+  fetchMobileDataUsage: offset => {
+    return new Promise((resolve, reject) => {
+      let endpoint =
+        'https://data.gov.sg/api/action/datastore_search?resource_id=a807b7ab-6cad-4aa6-87d0-e283a7353a0f&limit=45&offset=' +
+        offset;
+      return DomainCommon.fetch(endpoint, resolve, reject);
+    });
+  },
+};
+
+export default DataGovService;
