a2c7d932c9f39631189e79e1545e1d66e09243d4
agustinus <agustinus.kurniawan@reebonz.com>
2019-09-10 17:41:23 +0800

    Add onPress testing on GeneralModal component

--- src/Components/__tests__/GeneralModal.js
+++ src/Components/__tests__/GeneralModal.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import GeneralModal from '../GeneralModal';
+import Adapter from 'enzyme-adapter-react-16';
+import {configure, shallow} from 'enzyme';
 
 import ReactTestRenderer from 'react-test-renderer';
+
+configure({adapter: new Adapter()});
 
 describe('GeneralModal', () => {
   let wrapper = null;
 
   beforeAll(() => {
-    wrapper = ReactTestRenderer.create(<GeneralModal />);
+    const props = {isVisible: false};
+    wrapper = shallow(<GeneralModal {...props} />);
   });
 
-  test('renders correctly', () => {
-    expect(wrapper.toJSON()).toMatchSnapshot();
+  it('renders correctly', () => {
+    expect(
+      ReactTestRenderer.create(<GeneralModal />).toJSON(),
+    ).toMatchSnapshot();
+  });
+
+  it('should invoke _setModalVisible() when onPress', () => {
+    wrapper
+      .find('TouchableOpacity')
+      .props()
+      .onPress(true);
+    expect(wrapper.state().modalVisible).toEqual(true);
   });
 });
